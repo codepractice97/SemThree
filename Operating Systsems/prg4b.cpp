31f28e917956313d70529a798ea8f58bad26f72c
codepractice97 <mukulsingh1248@gmail.com>
2018-08-20 12:11:05 +0530

    OS prg4

---
+++ Operating Systsems/prg4b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+using namespace std;
+
+int main(){
+		pid_t a = fork();
+		cout << a << "\n";
+		if (a < 0)
+			cout << "New process not crated" << endl;
+		else if (a==0)
+			cout << "New Process: " << getpid() << endl;
+		else 
+			cout << "Old Process: " << getpid() << endl;
+			
+		return 0;
+}
