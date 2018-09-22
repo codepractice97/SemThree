6e558472b00004a01a65c435ae1841dea111d1d1
codepractice97 <mukulsingh1248@gmail.com>
2018-09-23 02:06:08 +0530

    Scheduling Done

--- Operating Systsems/Scheduling.cpp
+++ Operating Systsems/Scheduling.cpp
@@ -1,42 +1,36 @@
 #include <iostream>
+#include <deque>
 using namespace std;
 
 struct Process{
-	string p_name;
+	int p_name;
 	int b_time, arr_time, ta_time, wait_time, priority;
+	int progress;
 };
 
-struct Processes{
-	int size;
-	Process *p;
-	Processes(int s){
-		size = s;
-		p = new Process[s];
-	}
-	Process* proc(int i) { return &p[i]; }
-};
-
-void displayProcesses(Processes *P){
-	cout << "PID\tBurst Time\tArrival Time\t";
+void displayProcesses(deque <Process> &P){
+	cout << "PID\tBurst Time\tArrival Time\t\t";
 	cout << "Wait Time\tTurn Aroud Time\tPriority" << endl;
-	for (int i=0; i<P->size; i++){
-		cout << P->proc(i)->p_name << "\t\t";
-		cout << P->proc(i)->b_time << "\t\t";
-		cout << P->proc(i)->arr_time << "\t\t";
-		cout << P->proc(i)->wait_time << "\t\t";
-		cout << P->proc(i)->ta_time << "\t\t";
-		cout << P->proc(i)->priority << endl;
+	for (int i = 0; i < P.size(); i++){
+		Process process = P.at(i);
+		cout << process.p_name << "\t\t";
+		cout << process.b_time << "\t\t";
+		cout << process.arr_time << "\t\t";
+		cout << process.wait_time << "\t\t";
+		cout << process.ta_time << "\t\t";
+		cout << process.priority << endl;
 	}
 }
 
-void displayResults(Processes *P){
-	float avg_ta_time, avg_wait_time;
-	for (int i=0; i<P->size; i++){
-		avg_wait_time += P->proc(i)->wait_time;
-		avg_ta_time += P->proc(i)->ta_time;
+void displayResults(deque <Process> &P){
+	float avg_ta_time = 0.0f, avg_wait_time = 0.0f;
+	for (int i = 0; i < P.size(); i++){
+		Process process = P.at(i);
+		avg_wait_time += process.wait_time;
+		avg_ta_time += process.ta_time;
 	}
-	avg_ta_time /= P->size;
-	avg_wait_time /= P->size;
+	avg_ta_time /= P.size();
+	avg_wait_time /= P.size();
 	cout << "Average turn around time: " << avg_ta_time << endl;
 	cout << "Average waiting time: " << avg_wait_time << endl;
 }
@@ -44,100 +38,197 @@ void displayResults(Processes *P){
 class Scheduler{
 public:
 
-	void FCFS(Processes *P){
-		int time_spent = 0;
-		for (int i=0; i<P->size; i++){
-			calculateProcessExecution(P->proc(i), time_spent);
-		}
-		displayProcesses(P);
-		displayResults(P);
-	}
-
-	void SRJF(Processes *P){
+	void FCFS(deque <Process> &P){
 		sortJobsArrivalAscending(P);
 		int time_spent = 0;
-		for (int i=0; i< P->size ; i++){
-			int active_process_count = 0;
-			if (time_spent < P->proc(i)->arr_time){
-				 time_spent = P->proc(i)->arr_time;
-			}
-			int curr_arr_time = time_spent;
-			for (int j = i; j < P->size && 
-					P->proc(j)->arr_time <= curr_arr_time; j++ )
-				active_process_count++;
-			sortJobsBurstAscending(P, i, active_process_count + i);
-			calculateProcessExecution(&P->p[i], time_spent);
+		for (int i=0; i<P.size(); i++){
+			if (time_spent < P.at(i).arr_time)
+				 time_spent = P.at(i).arr_time;
+			calculateProcessExecution(P.at(i), time_spent, P.at(i).b_time);
 		}
 		displayProcesses(P);
 		displayResults(P);
 	}
-	
-	void SJF(Processes *P){
-		sortJobsBurstAscending(P, 0, P->size);
-		FCFS(P);
+
+	void SJF(deque <Process> &P){
+		sortJobsArrivalAscending(P);
+		int time_spent = 0;
+		for (int i=0; i < P.size();i++){
+			if (time_spent < P.at(i).arr_time)
+				 time_spent = P.at(i).arr_time;
+			int active_processes = 0;
+			for (int j = i; (j < P.size()) && (P.at(j).arr_time <= time_spent); j++){
+				active_processes++;
+			}
+			sortJobsRemainingBurstAscending(P, i, active_processes + i);
+			calculateProcessExecution(P.at(i), time_spent, P.at(i).b_time);
+		}
+		displayProcesses(P);
+		displayResults(P);
+	}
+
+	void SRJF(deque <Process> &P){
+		sortJobsArrivalAscending(P);
+		int time_spent = 0;
+		for (int i=0; i < P.size();i++){
+			if (time_spent < P.at(i).arr_time)
+				time_spent = P.at(i).arr_time;
+			int active_processes = 0, j;
+			for (j = i; j < P.size() && P.at(j).arr_time <= time_spent; j++)
+				active_processes++;
+			sortJobsRemainingBurstAscending(P, i, active_processes + i);
+
+			int burst_time = P.at(i).b_time;
+			// Find pre-emptive burst time
+			if (j < P.size()){
+				int active_processes2 = 0, time_spent_temp = P.at(j).arr_time;
+				for (int k = j; k < P.size() && P.at(k).arr_time <= time_spent_temp; k++)
+					active_processes2++;
+				sortJobsRemainingBurstAscending(P, j, active_processes2 + j);
+				burst_time = P.at(j).arr_time - time_spent;
+			}
+			if ( !calculateProcessExecution(P.at(i), time_spent, burst_time) )
+				i--; // Re process the process if not complete
+		}
+		displayProcesses(P);
+		displayResults(P);
+	}
+
+	void roundRobin(deque <Process> &P, int quantum){
+		sortJobsArrivalAscending(P);
+		int processes_complete = 0;
+		int time_spent = P.at(0).arr_time;
+		for (int i = 0; i < P.size(); i++){
+			if (time_spent < P.at(i).arr_time) {
+				 i = 0;
+			}
+			if (P.at(i).progress != P.at(i).b_time){
+				if ( calculateProcessExecution(P.at(i), time_spent, quantum) )
+					processes_complete++;
+			}
+			if ( i == P.size() - 1 && processes_complete < P.size() )
+				i = 0;
+		}
+		displayProcesses(P);
+		displayResults(P);
+	}
+
+	void priorityScheduling(deque <Process> &P){
+		sortPriority(P);
+		int time_spent = 0;
+		for (int i=0; i<P.size(); i++){
+			if (time_spent < P.at(i).arr_time)
+				 time_spent = P.at(i).arr_time;
+			calculateProcessExecution(P.at(i), time_spent, P.at(i).b_time);
+		}
+		displayProcesses(P);
+		displayResults(P);
+	}
+
+	void sortPriority(deque <Process> &P){
+		Process temp;
+		for(int i=0; i < P.size(); i++) {
+			for(int j=0; j < P.size() - 1; j++) {
+				if( P.at(j+1).priority < P.at(j).priority ){
+					temp = P.at(j+1);
+					P.at(j+1) = P.at(j);
+					P.at(j) = temp;
+				}
+			}
+		}
 	}
 	
-	void sortJobsBurstAscending(Processes *P, int start, int end){
+	void sortJobsRemainingBurstAscending(deque <Process> &P, int start, int end){
 		Process temp;
-		for(int i = start; i < end; i++) {
+		for(int i = start; i < end ; i++) {
 			for(int j = start; j < end - 1; j++) {
-				if( P->proc(j+1)->b_time < P->proc(j)->b_time ){
-					temp = P->p[j+1];
-					P->p[j+1] = P->p[j];
-					P->p[j] = temp;
+				if( P.at(j+1).b_time - P.at(j+1).progress < P.at(j).b_time - P.at(j).progress){
+					temp = P.at(j+1);
+					P.at(j+1) = P.at(j);
+					P.at(j) = temp;
 				}
 			}
 		}
 	}
 
-	void sortJobsArrivalAscending(Processes *P){
+	void sortJobsArrivalAscending(deque <Process> &P){
 		Process temp;
-		for(int i=0;i<P->size;i++) {
-			for(int j=0;j<P->size-1;j++) {
-				if( P->proc(j+1)->arr_time < P->proc(j)->arr_time ){
-					temp = P->p[j+1];
-					P->p[j+1] = P->p[j];
-					P->p[j] = temp;
+		for(int i=0; i < P.size(); i++) {
+			for(int j=0; j < P.size() - 1; j++) {
+				if( P.at(j+1).arr_time < P.at(j).arr_time ){
+					temp = P.at(j+1);
+					P.at(j+1) = P.at(j);
+					P.at(j) = temp;
 				}
 			}
 		}
 	}
 	
-	void calculateProcessExecution(Process *P, int &time_spent){
-		if (P->arr_time < time_spent){
-			P->wait_time = time_spent - P->arr_time;
-			P->ta_time = time_spent + P->b_time - P->arr_time;
-		}
-		else{
-			P->wait_time = 0;
-			P->ta_time = P->b_time;
-		}
-		time_spent += P->b_time;
+	bool calculateProcessExecution(Process &P, int &time_spent, int burst_cycle){
+		if ( P.progress + burst_cycle > P.b_time)
+			burst_cycle = P.b_time - P.progress;
+		time_spent += burst_cycle;
+
+		P.wait_time = time_spent - P.b_time - P.arr_time;
+		P.ta_time = time_spent - P.arr_time;
+
+		P.progress += burst_cycle;
+		// Return if process execution is complete
+		return (P.progress == P.b_time);
 	}
 	
 };
 
-Processes input(){
+deque <Process> input(){
 	cout << "Enter number of processes to enter: ";
 	int size;
 	cin >> size;
-	Processes processes(size);
+	deque <Process> processes;
 	for(int i=0;i<size;i++){
-		//cout << "Enter process name: ";
-		cin >> processes.proc(i)->p_name;
-		//cout << "Enter burst time: ";
-		cin >> processes.proc(i)->b_time;
-		//cout << "Enter arrival time: ";
-		cin >> processes.proc(i)->arr_time;
+		Process process;
+		process.p_name = i;
+		cout << "Enter Process " << i << " details" << endl;
+		cout << "Arrival time: ";
+		cin >> process.arr_time;
+		cout << "Burst time: ";
+		cin >> process.b_time;
+		process.priority = 0;
+		process.progress = 0;
+		processes.push_back(process);
 	}
 	return processes;
 }
 
 int main(){
-	Processes p = input();
+	deque <Process> processes = input();
 	
 	Scheduler S;
-	S.FCFS(&p);
-	S.SJF(&p);
-	S.SRJF(&p);
+	char choice;
+	cout << "Choose Scheduling Method: \n1. FCFS\n2. SJF\n3. SRJF\n";
+	cout << "4. Round Robin\n5. Priority\n";
+	cin >> choice;
+	if (choice == '1')
+		S.FCFS(processes);
+	else if (choice == '2')
+		S.SJF(processes);
+	else if (choice == '3')
+		S.SRJF(processes);
+	else if (choice == '4'){
+		cout << "Enter quantum size: ";
+		int q; cin >> q;
+		S.roundRobin(processes, q);
+	} else if (choice == '5'){
+		cout << "Enter priorities of processes\n";
+		cout << "Lower values corresponding to higher priority\n";
+		for(int i = 0; i < processes.size(); i++){
+			cout << "Enter Process " << i << " priority" << endl;
+			cin >> processes.at(i).priority;
+		}
+		S.priorityScheduling(processes);
+	} else {
+		cout << "Invalid Option, running SRJF scheduling\n";
+		S.SRJF(processes);
+	}
+
+	return 0;
 }
