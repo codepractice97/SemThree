9bb89beab971293dcfc73569602384c79509a414
codepractice97 <mukulsingh1248@gmail.com>
2018-10-31 13:57:52 +0530

    Sparse Matrix in work

--- Data Structures/SparseMatrix.cpp
+++ Data Structures/SparseMatrix.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 
 template <class Type>
-class DiagonalMatrix{
-
+class SparseMatrix{
+public:
 	int size;
 	Type *A;
-public:
-	DiagonalMatrix(int m, int n){
+	SparseMatrix(int m, int n, int s){
 		if (m != n || m < 0 || n < 0)
 			throw "Invalid Matrix size.";
-		size = m;
+		size = s;
 		A = new Type[size];
 	}
-	~DiagonalMatrix() { delete A; }
+	~SparseMatrix() { delete A; }
+	virtual void set(int i, int j, Type value);
+	virtual Type get(int i, int j);
+};
+
+template <class Type>
+class DiagonalMatrix: public SparseMatrix<Type>{
+
+public:
+	DiagonalMatrix(int m, int n): SparseMatrix<Type>(m, n, m){}
 
 	void set(int i, int j, Type value){
 		if (i < 0 || j < 0 || i > size - 1 || j > size - 1)
@@ -37,7 +45,7 @@ public:
 
 // Row Major data arrangement
 template <class Type>
-class LowerTriangularMatrix{
+class LowerTriangularMatrix: public SparseMatrix<Type> {
 	int size;
 	Type *A;
 public:
@@ -71,7 +79,7 @@ public:
 
 // Column Major Data Arrangement
 template <class Type>
-class UpperTriangularMatrix{
+class UpperTriangularMatrix: public SparseMatrix<Type> {
 	int size;
 	Type *A;
 public:
@@ -105,7 +113,7 @@ public:
 
 // Using lower traingle and row major data representation
 template <class Type>
-class SymmetricMatrix{
+class SymmetricMatrix: public SparseMatrix<Type> {
 	int size;
 	Type *A;
 public:
@@ -137,39 +145,44 @@ public:
 	}
 };
 
-void input(){
-
-}
-
 int manin(){
 	char choice;
 	do {
-		cout << "Enter choice: \n1. Diagonal Matrix\n";
-		cout << "2. Lower Triangular Matrix\n3. Upper Triangular Matrix\n"
-		cout << "4. Symmetric Matrix\n";
-		cin >> choice;
-		cout << "Enter row count of the matrix: ";
-		int i, j, temp;
-		cin >> i;
-		cout << "Enter Column count of the matrix: "
-		cin >> j;
-		cout << "Enter the matrix in row major form:\n";
-		if (choice == '1'){
-			DiagonalMatrix diagonalMatrix(i, j);
+		try {
+			cout << "Enter choice: \n1. Diagonal Matrix\n";
+			cout << "2. Lower Triangular Matrix\n3. Upper Triangular Matrix\n";
+			cout << "4. Symmetric Matrix\n";
+			cin >> choice;
+			cout << "Enter row count of the matrix: ";
+			int i, j, temp;
+			cin >> i;
+			cout << "Enter Column count of the matrix: ";
+			cin >> j;
+			cout << "Enter the matrix:\n";
+			SparseMatrix<int> *matrix;
+			DiagonalMatrix<int> diagonalMatrix(i,j);
+			if (choice == '1'){
+				matrix = &diagonalMatrix;
+			}
 			for (int k = 0;k < i; k++){
 				for (int l = 0;l < j;l++){
 					cin >> temp;
-					diagonalMatrix.set(k, l, temp);
+					matrix->set(k, l, temp);
 				}
 			}
 			cout << "Stored matrix: \n";
 			for (int k = 0;k < i; k++){
 				for (int l = 0;l < j;l++){
 					cin >> temp;
-					diagonalMatrix.set(k, l, temp);
+					matrix->get(k, l);
 				}
 			}
+		} catch (const char m[]){
+			cout << m << endl;
 		}
-	} while (chocie == 'Y' || chocie == 'y');
+		
+		cout << "Enter 'Y' to continue" << endl;
+		cin >> choice;
+	} while (choice == 'Y' || choice == 'y');
 	return 0;
 }
